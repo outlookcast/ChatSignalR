42811290dd0fb5a458c44b574c705452cfbb621d
Vinicius Sampaio <viniciuscastrosampaio@gmail.com>
2018-12-26 20:17:15 -0200

    first commit

---
+++ ChatSignalR/Models/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatSignalR.Models
+{
+    public class Message
+    {
+        public string UserName { get; set; }
+        public string MessageBody { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
